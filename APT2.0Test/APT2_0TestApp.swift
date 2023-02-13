3d05de6a0600a2f6e14fb4c46be672031ab0df10
Juan Gonzalez <57960644+1gonzalez@users.noreply.github.com>
2023-02-13 16:58:45 -0500

    Add files via upload

---
+++ APT2.0Test/APT2_0TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  APT2_0TestApp.swift
+//  APT2.0Test
+//
+//  Created by Juan on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct APT2_0TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
